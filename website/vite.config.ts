2fbfbb196346c54a1fa802f90197c02e54adc259
Volodia Kraplich <v1mkss.m+git@gmail.com>
2025-06-27 16:59:55 +0300

    Build Vue app and deploy to public directory
    - Clean up existing Vue app files in public/ - Copy the built Vue app to
    the public directory - Set base path for Vue app in vite.config.ts

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/linux/CrystalLinux/tooling/package-repository/",
   plugins: [vue()],
-})
+});
